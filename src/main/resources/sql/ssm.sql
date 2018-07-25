e523aeb229a1db74c953ae95fa2bb620e9a56f5b
lwrong <2277839278@qq.com>
2018-07-25 23:22:27 +0800

    表规范

--- src/main/resources/sql/ssm.sql
+++ src/main/resources/sql/ssm.sql
@@ -47,25 +47,25 @@ CREATE TABLE `test` (
 -- --------
 DROP TABLE IF EXISTS `user_info`;
 CREATE TABLE `user_info` (
-  `ID` bigint(8) unsigned NOT NULL AUTO_INCREMENT,
-  `USER_NAME` varchar(50) CHARACTER SET utf8mb4 NOT NULL COMMENT '用户名-作为唯一的区分',
-  `ALIAS` varchar(300) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '昵称',
-  `PASSWORD` varchar(50) CHARACTER SET utf8mb4 NOT NULL COMMENT '密码，后续用MD5加密',
-  `STATUS` varchar(20) CHARACTER SET utf8mb4DEFAULT NULL COMMENT '判断用户是否可用 0可用，1不可用',
-  `ROLE_KEY` varchar(50) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '角色控制',
-  `USR_DESC` varchar(500) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '用户描述',
-  `ADDRESS` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '用户地址',
-  `SEX` varchar(10) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '性别',
-  `EMAIL` varchar(50) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '电子邮件地址',
-  `MOBIL` varchar(30) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '手机号',
-  `ONLINE_STATE` varchar(10) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '判断是否在线',
-  `CREATE_DATE` timestamp NULL DEFAULT NULL COMMENT '创建时间',
-  `UPDATE_DATE` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '控制密码过期时期',
-  `LAST_LOG_TIME` timestamp NULL DEFAULT NULL COMMENT '最后一次登录时间',
-  `LOG_COUNT` int(4) DEFAULT NULL COMMENT '登录系统次数',
-  `SCORE` int(8) DEFAULT NULL COMMENT '积分系统，方便后续扩展',
-  `FLAG` int(2) DEFAULT NULL COMMENT '后续考虑',
-  PRIMARY KEY (`ID`)
+  `id` bigint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(50) CHARACTER SET utf8mb4 NOT NULL COMMENT '用户名-作为唯一的区分',
+  `alias` varchar(300) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '昵称',
+  `password` varchar(50) CHARACTER SET utf8mb4 NOT NULL COMMENT '密码，后续用MD5加密',
+  `status` varchar(20) CHARACTER SET utf8mb4DEFAULT NULL COMMENT '判断用户是否可用 0可用，1不可用',
+  `role_key` varchar(50) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '角色控制',
+  `usr_desc` varchar(500) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '用户描述',
+  `address` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '用户地址',
+  `sex` varchar(10) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '性别',
+  `email` varchar(50) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '电子邮件地址',
+  `mobil` varchar(30) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '手机号',
+  `online_state` varchar(10) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '判断是否在线',
+  `create_date` timestamp NULL DEFAULT NULL COMMENT '创建时间',
+  `update_date` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '控制密码过期时期',
+  `last_log_time` timestamp NULL DEFAULT NULL COMMENT '最后一次登录时间',
+  `log_count` int(4) DEFAULT NULL COMMENT '登录系统次数',
+  `score` int(8) DEFAULT NULL COMMENT '积分系统，方便后续扩展',
+  `flag` int(2) DEFAULT NULL COMMENT '后续考虑',
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------
@@ -73,30 +73,30 @@ CREATE TABLE `user_info` (
 -- --------
 DROP TABLE IF EXISTS `user_token`;
 CREATE TABLE `user_token` (
-  `ID` int(11) NOT NULL,
-  `USER_ID` int(11) NOT NULL COMMENT '和用户一对多',
-  `IDENTITY_TYPE` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '身份类型（手机、邮箱等）',
-  `CREDENTIAL` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '证书，比如微信的token',
-  `VERIFIED` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '是否已验证',
-  `BINDING_TIME` timestamp NULL DEFAULT NULL COMMENT '绑定时间',
-  PRIMARY KEY (`ID`)
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL COMMENT '和用户一对多',
+  `identity_type` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '身份类型（手机、邮箱等）',
+  `credential` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '证书，比如微信的token',
+  `verified` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '是否已验证',
+  `binding_time` timestamp NULL DEFAULT NULL COMMENT '绑定时间',
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 -- --------
 -- 用户积分表
 -- --------
 DROP TABLE IF EXISTS `lev_score`;
 CREATE TABLE `lev_score` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `MIN_SCORE` int(40) NOT NULL COMMENT '该等级最小积分',
-  `MAX_SCORE` int(40) NOT NULL COMMENT '该等级最大积分',
-  `LEVEL` int(5) NOT NULL COMMENT '等级控制',
-  PRIMARY KEY (`ID`)
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `min_score` int(40) NOT NULL COMMENT '该等级最小积分',
+  `max_score` int(40) NOT NULL COMMENT '该等级最大积分',
+  `level` int(5) NOT NULL COMMENT '等级控制',
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 -- 初始化积分规则
-INSERT INTO `lev_score` (`ID`, `MIN_SCORE`, `MAX_SCORE`, `LEVEL`) VALUES ('1', '0', '80', '1');
-INSERT INTO `lev_score` (`ID`, `MIN_SCORE`, `MAX_SCORE`, `LEVEL`) VALUES ('2', '101', '300', '2');
-INSERT INTO `lev_score` (`ID`, `MIN_SCORE`, `MAX_SCORE`, `LEVEL`) VALUES ('3', '301', '900', '3');
-INSERT INTO `lev_score` (`ID`, `MIN_SCORE`, `MAX_SCORE`, `LEVEL`) VALUES ('4', '901', '1800', '4');
+INSERT INTO `lev_score` (`id`, `min_score`, `max_score`, `level`) VALUES ('1', '0', '80', '1');
+INSERT INTO `lev_score` (`id`, `min_score`, `max_score`, `level`) VALUES ('2', '101', '300', '2');
+INSERT INTO `lev_score` (`id`, `min_score`, `max_score`, `level`) VALUES ('3', '301', '900', '3');
+INSERT INTO `lev_score` (`id`, `min_score`, `max_score`, `level`) VALUES ('4', '901', '1800', '4');
 
 
 -- --------
@@ -104,17 +104,17 @@ INSERT INTO `lev_score` (`ID`, `MIN_SCORE`, `MAX_SCORE`, `LEVEL`) VALUES ('4', '
 -- --------
 DROP TABLE IF EXISTS `user_money_detail`;
 CREATE TABLE `user_money_detail` (
-  `ID` bigint(8) unsigned NOT NULL AUTO_INCREMENT,
-  `USER_ID` bigint(8) NOT NULL COMMENT '对应的用户id',
-  `GOODS_NAME` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '商品名称',
-  `MONEY` double(10,0) DEFAULT NULL COMMENT '支出或收入的钱（元）',
-  `TYPE` varchar(50) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '花费类型，后期根据此进行统计',
-  `MONEY_STATUS` int(2) DEFAULT NULL COMMENT '判断是否是收入还是支出, 1支出，2 收入',
-  `DES` varchar(250) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '对此条记录的备注',
-  `CREATE_DATE` timestamp DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '录入系统时间',
-  `MAKE_DATE` timestamp DEFAULT NULL COMMENT '实际花费或入账的时间',
-  `DELETE_STATE` int(2) DEFAULT '0' COMMENT '删除数据时进行标记，0代表未删除，1代表删除',
-  PRIMARY KEY (`ID`)
+  `id` bigint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(8) NOT NULL COMMENT '对应的用户id',
+  `goods_name` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '商品名称',
+  `money` double(10,0) DEFAULT NULL COMMENT '支出或收入的钱（元）',
+  `type` varchar(50) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '花费类型，后期根据此进行统计',
+  `money_status` int(2) DEFAULT NULL COMMENT '判断是否是收入还是支出, 1支出，2 收入',
+  `des` varchar(250) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '对此条记录的备注',
+  `create_date` timestamp DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '录入系统时间',
+  `make_date` timestamp DEFAULT NULL COMMENT '实际花费或入账的时间',
+  `delete_state` int(2) DEFAULT '0' COMMENT '删除数据时进行标记，0代表未删除，1代表删除',
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------
