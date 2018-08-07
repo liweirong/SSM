b3acb57c70aa6c2d4e30c2328c099053f33c07ed
lwrong <2277839278@qq.com>
2018-08-07 16:34:24 +0800

    信息完善

--- src/main/java/com/iris/utils/Constants.java
+++ src/main/java/com/iris/utils/Constants.java
@@ -1,7 +1,14 @@
 package com.iris.utils;
 
+/**
+ * 存放常量，避免出现魔法值
+ * @author lwrong
+ * @Date 2018/8/7
+ * @Time 16:21
+ */
 public class Constants {
 
+    //接口返回的状态描述
     public final static String FAIL =  "false";
     public final static String SUCCESS =  "success";
 
